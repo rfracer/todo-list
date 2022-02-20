876fcafe81a8f685464e3fefda39d3857ce334f3
August Biadala <augustbiadala@gmail.com>
2022-02-20 18:00:56 +0100

    Basic functions: display tasks, add, remove, filter, change complete status

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-const openAddModalBtn = document.getElementById('add-open-modal');
 const addModal = document.getElementById('add-modal');
+const openAddModalBtn = document.getElementById('add-open-modal');
 const closeModalBtn = document.getElementById('modal-close-btn');
 
+const searchInput = document.getElementById('search-input');
 const tasksContainer = document.getElementById('todo-list');
 const taskTemplate = document.getElementById('template-task');
+const taskCount = document.getElementById('complete-count');
+const taskCountProgressBar = document.querySelector('[data-progress-bar]');
+
+const addTaskForm = document.getElementById('add-form');
 const addTaskBtn = document.getElementById('add-btn');
 const addTaskInput = document.getElementById('add-task-name');
 const addTaskPriority = document.querySelectorAll('input[name="priority"]');
@@ -18,15 +23,98 @@ const tasks = [
   },
 ];
 
-const addTask = () => {};
+const addTask = (e) => {
+  e.preventDefault();
+  const name = addTaskInput.value;
+  if (name === '') {
+    // showMessage('Empty task error', 'error');
+    return;
+  }
+  let priority;
 
-const render = (filter = '') => {
-  for (const task of tasks) {
+  for (var i = 0; i < addTaskPriority.length; i++) {
+    if (addTaskPriority[i].checked) {
+      priority = addTaskPriority[i].value;
+      break;
+    }
+  }
+  const newTask = {
+    id: Date.now(),
+    name: name,
+    priority: priority,
+    complete: false,
+  };
+  tasks.push(newTask);
+  closeAddModal();
+  clearInputs();
+  render();
+};
+
+const deleteTask = (e) => {
+  if (!e.target.classList.contains('close-btn-icon')) return;
+
+  const item = e.target.closest('.todo-list__item');
+  const itemId = item.dataset.id;
+
+  const index = tasks.findIndex((task) => {
+    return task.id === itemId;
+  });
+  tasks.splice(index, 1);
+  render();
+  console.log(tasks);
+
+  // if (!tasks.length) renderCompleteTaskCount();
+};
+
+const renderCompleteTaskCount = () => {
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => {
+    return task.complete === true;
+  }).length;
+  if (totalTasks) {
+    taskCount.innerHTML = `${completedTasks} / ${totalTasks} Completed`;
+    taskCountProgressBar.value = completedTasks;
+    taskCountProgressBar.max = totalTasks;
+  } else {
+    taskCount.innerHTML = `No tasks`;
+    taskCountProgressBar.value = 0;
+  }
+};
+
+const taskCheckboxHandler = (e) => {
+  if (e.target.tagName.toLowerCase() != 'input') return;
+  const item = e.target.closest('.todo-list__item');
+  const itemId = item.dataset.id;
+  item.classList.toggle('is-complete');
+
+  const index = tasks.findIndex((task) => {
+    return task.id == itemId;
+  });
+  tasks[index].complete = !tasks[index].complete;
+
+  renderCompleteTaskCount();
+};
+
+const filterTask = (e) => {
+  const searchValue = searchInput.value;
+  renderTasks(searchValue);
+};
+
+const renderTasks = (filter = '') => {
+  tasksContainer.innerHTML = '';
+  const filteredTasks = !filter
+    ? tasks
+    : tasks.filter((task) => {
+        return task.name.includes(filter);
+      });
+
+  for (const task of filteredTasks) {
     const taskElement = document.importNode(taskTemplate.content, true);
+    const taskListItem = taskElement.firstElementChild; //? list item li
     const checkboxInput = taskElement.querySelector('input');
+    taskListItem.dataset.id = task.id;
     checkboxInput.id = task.id;
     if (task.complete) {
-      const taskListItem = taskElement.firstElementChild; //?
       checkboxInput.checked = task.complete;
       taskListItem.classList.add('is-complete');
     }
@@ -52,12 +140,29 @@ const render = (filter = '') => {
   }
 };
 
-openAddModalBtn.addEventListener('click', () => {
-  addModal.classList.add('visible');
-});
+const render = () => {
+  renderCompleteTaskCount();
+  renderTasks();
+};
 
-closeModalBtn.addEventListener('click', () => {
+const closeAddModal = () => {
   addModal.classList.remove('visible');
-});
+};
+
+const openAddModal = () => {
+  addModal.classList.add('visible');
+};
+
+const clearInputs = () => {
+  addTaskInput.value = '';
+  searchInput.value = '';
+};
 
 render();
+
+openAddModalBtn.addEventListener('click', openAddModal);
+closeModalBtn.addEventListener('click', closeAddModal);
+addTaskForm.addEventListener('submit', addTask);
+tasksContainer.addEventListener('click', deleteTask);
+tasksContainer.addEventListener('click', taskCheckboxHandler);
+searchInput.addEventListener('keyup', filterTask);
